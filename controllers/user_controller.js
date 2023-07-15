014293ba3907f103428fd7349d8f9b9d7518e2ac
Aditya Panday <aditya.pandey.1986.ad@gmail.com>
2023-07-15 09:54:52 +0530

    formed partials and layout

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req , res){
-    return res.end('<h1>srever connected to user controller</h1>');
+    return res.render('user', {
+        title:'profile'
+     });
 };

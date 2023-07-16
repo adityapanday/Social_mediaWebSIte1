14cc63b34e8c7ccbe7481f9de52a04dc33c325f9
Aditya Panday <aditya.pandey.1986.ad@gmail.com>
2023-07-16 20:49:50 +0530

    made form for sign in/out

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,25 @@ module.exports.profile = function(req , res){
         title:'profile'
      });
 };
+
+
+module.exports.signup = function(req, res){
+    return res.render('user_sign_up' ,{
+        title:'signup'
+    });
+}
+
+module.exports.signin = function(req, res){
+    return res.render('user_sign_in' ,{
+        title:'signup'
+    });
+}
+
+module.exports.create = function(req , res){
+  //todo later
+};
+
+module.exports.createSession = function(req , res){
+    //todo later
+  };
+  

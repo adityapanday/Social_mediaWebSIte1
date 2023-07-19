5ec0322b28c6fabc4346966fe25d4fa8c305e829
Aditya Panday <aditya.pandey.1986.ad@gmail.com>
2023-07-19 13:52:47 +0530

    finaly done with passport js

--- controllers/home_controler.js
+++ controllers/home_controler.js
@@ -1,11 +1,9 @@
-// const cookieParser = require("cookie-parser");
-
 module.exports.home = function(req, res){
-   console.log(req.cookies);
-   res.cookie('user_id', 25);
-   return res.render('home', {
-       title: "Home"
-   });
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
 // module.exports.actionName = function(req, res){}

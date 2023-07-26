23be0ce36284fdd681d7a74a86b1fea0799503a0
Aditya Panday <aditya.pandey.1986.ad@gmail.com>
2023-07-26 13:45:09 +0530

    added comment yet to fetch name of comenter

--- controllers/comment_controler.js
+++ controllers/comment_controler.js
@@ -1,20 +1,91 @@
 
-const Comment = require('../models/comment');
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 const  User  = require('../models/user');
 
+// module.exports.create = async(req, res)=>{
+//    const post = await Post.findById(req.body.post);
+//     //   res.send('post mila kya ');
+//     // res.send('comment ' , Comment.content);
+//         if (post){
+//             Comment.create({
+//                 comments: req.body.comments,
+//                 user: req.user._id,
+//                 post: req.body.post
+                
+//             }, function(err, comment){
+//                 // handle error
 
-module.exports.create = async(req , res)=>{
-    try {
-        const posts = await Comment.create({
-            comments : req.body.comments,
-            user: req.user._id
-            
-        });
+//                 post.comments.push(comment);
+//                 post.save();
 
-        res.redirect('back');
-    } catch (err) {
-        console.log('Error in storing comment:', err);
-        return ;
+//                 res.redirect('/');
+//             });
+//         };
+// };
+
+
+module.exports.create = async(req, res) => {
+   try{
+    let post = await Post.findById(req.body.post);
+    if (post) {
+      let comment = await Comment.create({
+        comments: req.body.comments,
+        user: req.user._id,
+        post: req.body.post
+      });
+      post.comments.push(comment);
+       post.save();
+      res.redirect('/');
+    } 
+} catch (err) {
+      res.send('Post not found');
+      console.log("error" , err);
+      return ;
     }
-}
+  };
+// module.exports.create = async(req , res)=>{
+//     try {
+//         const posts = await Comment.create({
+//             comments : req.body.comments,
+//             user: req.user._id,
+//             posts :req.body.post
+            
+//         });   
+
+//         res.redirect('back');
+//     } catch (err) {
+//         console.log('Error in storing comment:', err);
+//         return ;
+//     }
+// }
+
+
+
+
+
+
+// module.exports.create = async (req, res) => {
+//     try {
+//       const post = await Post.findById(req.body.post).populate(post);
+      
+//       if (post) {
+//         const comment = await Comment.create({
+//           content: req.body.content,
+//           post: req.body.post,
+//           user: req.user._id,
+//         });
+  
+//         post.comments.push(comment);
+//         await post.save();
+//         res.redirect("/");
+//       } else {
+//         console.log("Post not found");
+       
+//       }
+//     } catch (err) {
+//       console.log("Error in comment_controller:", err);
+      
+//     }
+//   };
--- controllers/home_controler.js
+++ controllers/home_controler.js
@@ -1,6 +1,86 @@
-const Comment = require('../models/comment');
+
 const Post = require('../models/post');
-const  User  = require('../models/user');
+const User = require('../models/user');
+const Comment = require('../models/comment');
+
+// module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
+
+    // populate the user of each post
+    //yahi use ho reahi hai on25 07 23
+//     Post.find({}).populate('user').populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     })
+//     .exec(function(err, posts){
+//         return res.render('home', {
+//             title: "Codeial | Home",
+//             posts:  posts
+//         });
+//     })
+
+// }
+
+
+
+// module.exports.create = async (req, res) => {
+  
+//     try {
+//       const post = await Post.find({}).populate('user')
+//         .populate({
+//           path: 'comments',
+//           populate: {
+//             path: 'user',
+//           }
+//         });
+//         // .exec();
+//         // let users = await User.find({});
+//      return res.render('home', {
+//         title: 'Codeial | Home',
+//         posts: post,
+//         comments :Comment
+//       });
+//     } catch (err) {
+//       console.log('Error in fetching posts:', err);
+//      return;
+//     }
+//   };
+  
+  
+  
+  
+  
+// module.exports.create = async (req, res) => {
+//   try {
+//     const posts = await Post.find({})
+//       .populate('user')
+//       .populate({
+//         path: 'comments',
+//         populate: {
+//           path: 'user',
+//         }
+//       });
+
+//     return res.render('home', {
+//       title: 'Codeial | Home',
+//       posts: posts,
+//     });
+//   } catch (err) {
+//     console.log('Error in fetching posts:', err);
+//     return res.status(500).send('An error occurred while fetching posts.');
+//   }
+// };
+  
 
 
 
@@ -9,7 +89,7 @@ const  User  = require('../models/user');
 module.exports.home = async (req, res) => {
     
     try {
-        const posts = await Post.find().populate('user') // Use exec() without a callback
+        const posts = await Post.find().populate('user').populate('comments') // Use exec() without a callback
           
         return res.render('home', {
             title: 'Home',
